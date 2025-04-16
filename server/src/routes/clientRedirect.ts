662b382ff47e508730b63df7b83310fce2222255
Raybul <egorraybul@gmail.com>
2025-04-16 17:53:59 +0300

    upd

--- server/src/routes/clientRedirect.ts
+++ server/src/routes/clientRedirect.ts
@@ -7,12 +7,22 @@ function clientRedirect() {
     const router = express.Router();
 
     router.post('/clientRedirect', async (req, res) => {
-        const { url, method, params, body } = req.body;
+        const { url, method, params, data } = req.body;
 
         switch (method) {
             case 'GET':
                 try {
                     const extRes = await axios.get(url, { params });
+                    res.status(200).send(extRes.data);
+                } catch (e) {
+                    res.status(500).end(e.message);
+                }
+
+                break;
+
+            case 'POST':
+                try {
+                    const extRes = await axios.post(url, data);
                     res.status(200).send(extRes.data);
                 } catch (e) {
                     res.status(500).end(e.message);
